9f480ba2617b4e6e25efba3201f90bdf38d50563
YiheWang <36209840+YiheWang@users.noreply.github.com>
2019-02-05 21:04:39 -0600

    design2 finished

--- Project1Code.ino
+++ Project1Code.ino
@@ -1,5 +1,5 @@
 #include <Servo.h>
-#include "SCoop.h"
+//#include "SCoop.h"
 
 int IN3 = 8;
 int IN4 = 7;
@@ -21,26 +21,33 @@ void setup()
   pinMode(motor2_enable, OUTPUT);
   pinMode(LED,OUTPUT);
   digitalWrite(LED,LOW);//initialize
-  myservo.attach(9, 600, 2300); // pin, min, max
+  myservo.attach(9); // pin, min, max
   Serial.begin(9600);
 }
 
 void loop()
 {
-  design1();
-  //design2();
-  //temperatureSensor();
-  //ventControl();
-  //servoControl();
+  //design1();  // vent completely open
+  design2(); // fan at a constant speed
 }
 
 void design1() // first requirement
 {
   // open the vent completely by server motor
-  servoControlOpen();
+  myservo.write(35);
   
   float temperature = temperatureSensor();
   fanControl(temperature);
+}
+
+void design2()
+{
+  digitalWrite(IN3, HIGH);
+  digitalWrite(IN4, LOW);
+  analogWrite(motor2_enable,100); // fan at a constant speed
+
+  float temperature = temperatureSensor();
+  servoControl(temperature);
 }
 
 float temperatureSensor()
@@ -51,22 +58,39 @@ float temperatureSensor()
   R = R0*R;
 
   float temperature = 1.0/(log(R/R0)/B+1/298.15)-273.15; // convert to temperature via datasheet
-
-  /*Serial.print("temperature = ");
-  Serial.println(temperature);
-
-   delay(1000);*/
-   return temperature;
+  return temperature;
 }
 
 void servoControlOpen()
 {
-  myservo.write(0);// open the vent
+  //myservo.write(80);
+  //delay(500);
+  myservo.write(35);// open the vent
+  //delay(500);
 }
 
 void servoControlClose()
 {
-  myservo.write(90);//close the vent
+  //myservo.write(80);
+  //delay(500);
+  myservo.write(130);//close the vent
+  //delay(500);
+}
+
+void servoControl(float temperature)
+{
+  if(temperature >= 29){
+    Serial.print("LED on!\n");
+    digitalWrite(LED,HIGH);
+    Serial.print("temperature = ");
+    Serial.println(temperature);
+    servoControlOpen(); //open vent when temperature higher than 27
+  }
+  else{
+    Serial.print("LED off!\n");
+    digitalWrite(LED,LOW);
+    servoControlClose();
+  }
 }
 
 void fanControl(float temperature)
@@ -84,16 +108,16 @@ void fanControl(float temperature)
       analogWrite(motor2_enable,80); // speed should be in range 0-255
     }*/
     if(temperature < 29 && temperature >=27){
-      analogWrite(motor2_enable,30);
-    }
-    else if(temperature < 31 && temperature >= 29){
       analogWrite(motor2_enable,70);
     }
-    else if(temperature < 33 && temperature >= 31){
+    else if(temperature < 31 && temperature >= 29){
       analogWrite(motor2_enable,110);
     }
-    else if(temperature <35 && temperature >=33){
+    else if(temperature < 33 && temperature >= 31){
       analogWrite(motor2_enable,150);
+    }
+    else if(temperature <35 && temperature >=33){
+      analogWrite(motor2_enable,170);
     }
     else if(temperature <37 && temperature >=35){
       analogWrite(motor2_enable,190);
